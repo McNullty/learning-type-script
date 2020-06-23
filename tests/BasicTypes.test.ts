225260c556cb225fc27cc078897a9235960480ac
Mladen Čikara <mladen.cikara@gmail.com>
2020-06-23 12:42:34 +0200

    Tests for number type

--- tests/BasicTypes.test.ts
+++ tests/BasicTypes.test.ts
@@ -4,4 +4,16 @@ describe('Basic Types examples', function() {
 
         expect(isDone).toBeFalsy();
     });
+
+    it('Number type', function () {
+        let decimal: number = 6;
+        let hex: number = 0xf00d;
+        let binary: number = 0b1010;
+        let octal: number = 0o744;
+
+        expect(decimal).toBe(6);
+        expect(hex).toBe(61453);
+        expect(binary).toBe(10);
+        expect(octal).toBe(484);
+    });
 })
